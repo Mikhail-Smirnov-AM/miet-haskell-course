ebdbbc1c6f50f71bae08294246b2e06d109c0499
SmirnovM <misha-smirnov@inbox.ru>
2024-03-10 16:30:45 +0300

    Add Lab1 solution

--- Lab1/src/Lists.hs
+++ Lab1/src/Lists.hs
@@ -10,7 +10,10 @@ newtype Point = Point [Double] deriving (Eq, Show, Read)
 
 -- используйте рекурсию и сопоставление с образцом
 distance :: Point -> Point -> Double
-distance x y = error "todo"
+distance (Point []) (Point []) = 0
+distance x (Point []) = error "Different length"
+distance (Point []) y = error "Different length"
+distance (Point (x0 : xs)) (Point (y0 : ys)) = sqrt ((x0-y0)**2 + (distance (Point xs) (Point ys))**2)
 
 -- intersect xs ys возвращает список, содержащий общие элементы двух списков.
 -- intersect [1, 2, 4, 6] [5, 4, 2, 5, 7] == [2, 4] (или [4, 2]!)
@@ -18,14 +21,18 @@ distance x y = error "todo"
 
 -- используйте рекурсию и сопоставление с образцом
 intersect :: [Integer] -> [Integer] -> [Integer]
-intersect xs ys = error "todo"
+intersect xs [] = []
+intersect [] ys = []
+intersect (x:xs) ys = if elem x ys then x:(intersect xs ys) else intersect xs ys
 
 -- zipN принимает список списков и возвращает список, который состоит из
 -- списка их первых элементов, списка их вторых элементов, и так далее.
 -- zipN [[1, 2, 3], [4, 5, 6], [7, 8, 9]] == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
 -- zipN [[1, 2, 3], [4, 5], [6]] == [[1, 4, 6], [2, 5], [3]]
 zipN :: [[a]] -> [[a]]
-zipN xss = error "todo"
+zipN [] = []
+zipN ([]:xss) = zipN xss
+zipN ((x:xs) : xss) = (x:[h | (h:_) <- xss]):(zipN(xs:[t | (_:t) <- xss]))
 
 -- Нижеперечисленные функции можно реализовать или рекурсивно, или с помощью
 -- стандартных функций для работы со списками (map, filter и т.д.)
@@ -37,15 +44,28 @@ zipN xss = error "todo"
 -- find (> 0) [-1, 2, -3, 4] == Just 2
 -- findLast (> 0) [-1, 2, -3, 4] == Just 4
 -- find (> 0) [-1, -2, -3] == Nothing
-find, findLast :: (a -> Bool) -> [a] -> Maybe a
-find f xs = error "todo"
-findLast f xs = error "todo"
+find :: (a -> Bool) -> [a] -> Maybe a
+find _ [] = Nothing
+find f (x:xs) = if f x then Just x else find f xs
+
+find_filter :: (a -> Bool) -> [a] -> Maybe a
+find_filter f xs = if null filter_x 
+                   then Nothing
+                   else Just (filter_x !! 0)
+                   where filter_x = filter f xs
+
+findLast :: (a -> Bool) -> [a] -> Maybe a
+findLast f xs = if null filter_x 
+                then Nothing
+                else Just (filter_x !! ((length filter_x) - 1))
+                where filter_x = filter f xs
 
 -- mapFuncs принимает список функций fs и возвращает список результатов 
 -- применения всех функций из fs к x.
 -- mapFuncs [\x -> x*x, (1 +), \x -> if even x then 1 else 0] 3 == [9, 4, 0]
 mapFuncs :: [a -> b] -> a -> [b]
-mapFuncs fs x = error "todo"
+mapFuncs [] x = []
+mapFuncs (f:fs) x = (f x):(mapFuncs fs x)
 
 -- satisfiesAll принимает список предикатов (функций, возвращающих Bool) preds
 -- и возвращает True, если все они выполняются (т.е. возвращают True) для x.
@@ -53,7 +73,9 @@ mapFuncs fs x = error "todo"
 -- satisfiesAll [even, \x -> x rem 5 == 0] 10 == True
 -- satisfiesAll [] 4 == True (кстати, почему?)
 satisfiesAll :: [a -> Bool] -> a -> Bool
-satisfiesAll preds x = error "todo"
+satisfiesAll [] _ = True
+satisfiesAll (pred0:preds) x = if pred0 x then (satisfiesAll preds x) else False
+
 
 -- Непустой список состоит из первого элемента (головы)
 -- и обычного списка остальных элементов
@@ -62,8 +84,19 @@ data NEL a = NEL a [a] deriving (Eq, Show, Read)
 
 -- Запишите правильный тип (т.е. такой, чтобы функция имела результат для любых аргументов
 -- без вызовов error) и реализуйте функции на NEL, аналогичные tail, last и zip
--- tailNel :: NEL a -> ???
--- lastNel :: NEL a -> ???
--- zipNel :: NEL a -> NEL b -> ???
--- listToNel :: [a] -> ???
--- nelToList :: NEL a -> ???
+tailNel :: NEL a -> [a]
+tailNel (NEL x xs) = xs
+
+lastNel :: NEL a -> a
+lastNel (NEL x []) = x
+lastNel (NEL x (x0:xs)) = lastNel(NEL x0 xs)
+
+zipNel :: NEL a -> NEL b -> NEL (a,b)
+zipNel x y = listToNel (zip (nelToList x) (nelToList y))
+
+listToNel :: [a] -> NEL a
+listToNel [] = error "Cannot convert empty list to NEL"
+listToNel (x:xs) = NEL x xs
+
+nelToList :: NEL a -> [a]
+nelToList (NEL x xs) = x:xs
--- Lab1/test/Spec.hs
+++ Lab1/test/Spec.hs
@@ -15,20 +15,84 @@ main = hspec $ do
         it "max3" $ do
             max3 1 3 2 `shouldBe` 3
             max3 5 2 5 `shouldBe` 5
-        it "median3" pending
-        it "rbgToCmyk" pending
-        it "geomProgression" pending
-        it "coprime" pending
+        it "median3" $ do
+            median3 1 2 3 `shouldBe` 2
+            median3 5 4 6 `shouldBe` 5
+            median3 10 35 22 `shouldBe` 22
+        it "rbgToCmyk" $ do
+            rgbToCmyk (RGB 0 0 0) `shouldBe` (CMYK 0.0 0.0 0.0 1.0)
+            rgbToCmyk (RGB 255 0 0) `shouldBe` (CMYK 0.0 1.0 1.0 0.0)
+            rgbToCmyk (RGB 0 255 0) `shouldBe` (CMYK 1.0 0.0 1.0 0.0)
+            rgbToCmyk (RGB 0 0 255) `shouldBe` (CMYK 1.0 1.0 0.0 0.0)
+            rgbToCmyk (RGB 255 255 0) `shouldBe` (CMYK 0.0 0.0 1.0 0.0)
+            rgbToCmyk (RGB 255 0 255) `shouldBe` (CMYK 0.0 1.0 0.0 0.0)
+            rgbToCmyk (RGB 0 255 255) `shouldBe` (CMYK 1.0 0.0 0.0 0.0)
+            rgbToCmyk (RGB 255 255 255) `shouldBe` (CMYK 0.0 0.0 0.0 0.0)
+            rgbToCmyk (RGB 100 150 200) `shouldBe` (CMYK 0.5000000000000001 0.24999999999999997 0.0 0.21568627450980393)
+        it "geomProgression" $ do
+            geomProgression 5.0 1.0 0 `shouldBe` 5.0
+            geomProgression 5.0 0.5 1 `shouldBe` 2.5
+            geomProgression 5.0 0.5 2 `shouldBe` 1.25
+            geomProgression 5.0 1.5 1 `shouldBe` 7.5
+            geomProgression 5.0 1.5 2 `shouldBe` 11.25
+            geomProgression 0.0 1.5 10 `shouldBe` 0
+            geomProgression 2.0 0.0 5 `shouldBe` 0
+        it "coprime" $ do 
+            coprime 2 3 `shouldBe` True
+            coprime 8 9 `shouldBe` True
+            coprime 10 15 `shouldBe` False
+            coprime (-2) 3 `shouldBe` True
+            coprime 8 (-9) `shouldBe` True
+            coprime (-10) (-15) `shouldBe` False
     describe "lists" $ do
-        it "distance" pending
-        it "intersect" pending
-        it "zipN" pending
-        it "find" pending
-        it "findLast" pending
-        it "mapFuncs" pending
-        it "tailNel" pending
-        it "lastNel" pending
-        it "zipNel" pending
-        it "listToNel" pending
-        it "nelToList" pending
-    describe "luhn" $ it "" pending
+        it "distance" $ do
+            distance (Point [1,2,3]) (Point [1,2,3]) `shouldBe` 0.0
+            distance (Point [1,2,2]) (Point [1,2,3]) `shouldBe` 1.0
+            distance (Point [1,2,3]) (Point [2,2,2]) `shouldBe` sqrt 2
+        it "intersect" $ do
+            intersect [1, 2, 4, 6] [5, 4, 2, 5, 7] `shouldBe` [2, 4]
+            intersect [1, 2, 4, 6] [3, 5, 7] `shouldBe` []
+            intersect [] [1] `shouldBe` []
+            intersect [1] [] `shouldBe` []
+        it "zipN" $ do
+            zipN [[1, 2, 3], [4, 5, 6], [7, 8, 9]] `shouldBe` [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+            zipN [[1, 2, 3], [4, 5], [6]] `shouldBe` [[1, 4, 6], [2, 5], [3]]
+            zipN [[1], [2,3], [4,5,6]] `shouldBe` [[1, 2, 4],[3,5],[6]]
+            zipN [[1, 2, 3], [4, 5], []] `shouldBe` [[1,4],[2,5],[3]]
+        it "find" $ do
+            find (> 0) [-1, 2, -3, 4] `shouldBe` Just 2
+            find (> 0) [-1, -2, -3] `shouldBe` Nothing
+            find (even) [-1, 5, -3, 4] `shouldBe` Just 4
+            find (even) [-1, 3, -5] `shouldBe` Nothing
+        it "findLast" $ do
+            findLast (> 0) [-1, 2, -3, 4] `shouldBe` Just 4
+            findLast (> 0) [-1, -2, -3] `shouldBe` Nothing
+            findLast (even) [-1, 2, -3, 4] `shouldBe` Just 4
+            findLast (even) [-1, 3, -5] `shouldBe` Nothing
+        it "mapFuncs" $ do
+            mapFuncs [\x -> x*x, (1 +), \x -> if even x then 1 else 0] 3 `shouldBe` [9, 4, 0]
+        it "tailNel" $ do
+            tailNel (NEL 1 [2,3]) `shouldBe` [2,3]
+            tailNel (NEL "a" ["b","c","d"]) `shouldBe` ["b","c","d"]
+            tailNel (NEL 1 []) `shouldBe` []
+        it "lastNel" $ do
+            lastNel (NEL 1 [2,3]) `shouldBe` 3
+            lastNel (NEL "a" ["b","c","d"]) `shouldBe` "d"
+            lastNel (NEL 1 []) `shouldBe` 1
+        it "zipNel" $ do
+            zipNel (NEL 1 [2,3]) (NEL 4 [5,6]) `shouldBe` (NEL (1,4) [(2,5),(3,6)])
+        it "listToNel" $ do 
+            listToNel [1,2,3] `shouldBe` (NEL 1 [2,3])
+            listToNel [1] `shouldBe` (NEL 1 [])
+        it "nelToList" $ do
+            nelToList (NEL 1 [2, 3]) `shouldBe` [1,2,3]
+            nelToList (NEL 1 []) `shouldBe` [1]
+    describe "luhn" $ do
+        it "isLuhnValid" $ do
+            isLuhnValid 1111222233334444 `shouldBe` True
+            isLuhnValid 4561261212345467 `shouldBe` True
+            isLuhnValid 1111222233334 `shouldBe` True
+            isLuhnValid 4561261212345464 `shouldBe` False
+            isLuhnValid 1112222233334444 `shouldBe` False
+            isLuhnValid 11112222333344 `shouldBe` False
+            isLuhnValid (-1) `shouldBe` False
